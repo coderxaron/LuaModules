ec068e817fdf7f9caa52b63b30b3d0a2363033b5
coderxaron <schnitzelwolke@gmail.com>
2025-07-03 15:58:50 +0200

    Add files via upload

---
+++ SlangModule.lua
@@ -0,0 +1,38 @@
+--[[ 
+                                                                    C O D E R X A R O N   2 0 2 5   F R A M E W O R K
+
+    Example Usage: 
+    
+    local Coolplace = game:GetService("ReplicatedStorage")
+    local Ohio = require(Coolplace:WaitForChild("SlangModule"))
+
+    Ohio.Yap("I Have RIZZ")
+
+    while Ohio.Truerizz do
+    	Ohio.Chill(1)
+    	Ohio.Yap("This is a Loop in Slang")
+    end
+
+--]]
+
+local slang = {}
+
+local Yap = print
+local Chill = wait
+local Truerizz = true
+local Fakerizz = false
+local Zzz = task.wait
+local sigmainohio = 1
+
+slang.Truerizz = Truerizz
+slang.Fakerizz = Fakerizz
+slang.Yap = Yap
+slang.Chill = Chill
+slang.Zzz = Zzz
+slang.Random = math.random
+slang.Clock = os.clock
+slang.Time = os.time
+slang.Date = os.date
+slang.SigmaInOhio = sigmainohio
+
+return slang
